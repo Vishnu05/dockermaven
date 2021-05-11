65e1a3230dbea45563401aab6e6a40e8aecbdd5b
Vishnu T <vishnuthamiz96@gmail.com>
2021-05-11 20:07:27 +0530

    initial

---
+++ src/main/java/com/demomavendocker/DemomavendockerApplication.java
@@ -0,0 +1,14 @@
+package com.demomavendocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemomavendockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemomavendockerApplication.class, args);
+		System.out.println("-------------------------------------- hello ----------------------------");
+	}
+
+}
---
+++ src/test/java/com/demomavendocker/DemomavendockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demomavendocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemomavendockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
